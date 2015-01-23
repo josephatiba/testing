18ba5d53c860a557efc7ab48dae304f25f84e8f7
Joey Atiba <joey.atiba@gmail.com>
2015-01-22 16:55:04 -0800

    test if the factory is valid

---
+++ spec/factories/books.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :book do
+    title "Novel"
+    author_name "Joey"
+    subject "programming"
+    genre "non-fiction"
+    summary "Cool book"
+    pagecount 1
+  end
+
+end
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Book, :type => :model do
+  it "has a valid factory" do 
+    expect(FactoryGirl.build(:book)).to be_valid
+  end
+
+
+
+end
