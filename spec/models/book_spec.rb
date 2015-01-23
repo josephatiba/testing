2572fc30cf2b9631487094a487a90e58d15ca49d
Joey Atiba <joey.atiba@gmail.com>
2015-01-22 17:10:23 -0800

    tests if the book is invalid without an author name

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe Book, :type => :model do
     expect(book).to be_invalid
   end
 
+  it "is invalid without an author_name" do 
+    book = FactoryGirl.build(:book, author_name: nil)
+    expect(book).to be_invalid
+  end
+
 
 
 end
